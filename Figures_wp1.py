78674b1f325bba45153fc1c64d162db5e66623ec
MARQyie <50098649+MARQyie@users.noreply.github.com>
2019-09-04 17:17:34 +0200

    Commit #2

--- Figures_wp1.py
+++ Figures_wp1.py
@@ -18,16 +18,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 import seaborn as sns
 sns.set(style='white',font_scale=1.5)
+sns.set_palette('Greys')
+sns.set_context('notebook')
 
 import os
 os.chdir(r'X:\My Documents\PhD\Materials_dissertation\2-Chapter_2-bank_char')
 
 #------------------------------------------
 # Load df
-df = pd.read_csv('df_wp1_clean.csv', index_col = 0 )
+df = pd.read_csv('df_wp1_clean.csv', index_col = 0)
 
 # Make multi index
 df.set_index(['IDRSSD','date'],inplace=True)
@@ -36,3 +37,16 @@ df.set_index(['IDRSSD','date'],inplace=True)
 # Split the dataframes on securitizer and non-securitizers
 df_sec = df[df.RCBtot > 0]
 df_nonsec = df[df.RCBtot == 0]
+
+#------------------------------------------
+# Figure 1: Total securitization (B705 + B711; 1-4 Family residential loans + all other)
+## Sum per year, drop index level and divide by $1 million
+sec_year = df_sec.RCB705.mean(level = [1,1])
+sec_year = sec_year.droplevel(level = 0)
+sec_year = sec_year.divide(1e6)
+
+##plot
+fig, ax = plt.subplots(figsize=(12, 8))
+plt.title('Total Securitization of Residential Loans')
+ax.set(xlabel='Year', ylabel = 'Amount of securitization (in $ Million)')
+ax.plot(sec_year)
